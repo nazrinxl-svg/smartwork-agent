85ec73d2f8274ece68ce3eec2b3213634ebcd108
Naz <nazrinxl@gmail.com>
2026-06-15 09:56:45 +0700

    Fix SmartWork Firebase config for Vercel auth

---
+++ public/firebase-config.js
@@ -0,0 +1,8 @@
+﻿window.SMARTWORK_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBOlTcGcZ3uwdXnhkvCLxnF_oNucELZCaw",
+   authDomain: "smartwork-agent.firebaseapp.com",
+  projectId: "smartwork-agent",
+  storageBucket: "smartwork-agent.appspot.com",
+  messagingSenderId: "838957667960",
+  appId: "1:838957667960:web:e8d791f9504248aecfff8d"
+};
